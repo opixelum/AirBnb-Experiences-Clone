00614504d5ec178959d4c38bb2d817183422fbfa
Opixelum <12w0zvkgazd3@opayq.net>
2022-02-06 23:57:26 +0100

    Added hero

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import photoGrid from "../images/photo-grid.png"
+
+export default function Hero() {
+    return (
+        <div className="hero-div">
+          <img alt="hero-img" src={photoGrid}></img>
+          <h1>Online Experiences</h1>
+          <p>Join unique interactive activities led by one-of-a-kind-hosts-all
+          without leaving home.</p>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import airbnbLogo from "../images/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+          <img alt="AirBnb logo" src={airbnbLogo}></img>
+        </nav>
+    )
+}
